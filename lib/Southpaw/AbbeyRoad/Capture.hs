387676684d3680ee04d15da0fb094cf474deab8c
Jonatan H Sundqvist <jonatanhsundqvist@gmail.com>
2015-07-22 07:30:46 +0200

    More refactoring

--- lib/Southpaw/AbbeyRoad/Capture.hs
+++ lib/Southpaw/AbbeyRoad/Capture.hs
@@ -26,31 +26,32 @@ module Southpaw.AbbeyRoad.Capture where
 ---------------------------------------------------------------------------------------------------
 -- We'll need these
 ---------------------------------------------------------------------------------------------------
-import Control.Concurrent (threadDelay)
-import Control.Exception (bracket, finally)
-import Control.Monad (unless, when)
-import Data.Int
-import Data.List (intersperse)
-import Data.Word
-import Foreign
-import Foreign.C.Types
-import Foreign.ForeignPtr
-import Foreign.ForeignPtr.Unsafe
-import Foreign.Ptr
-import Sound.ALUT
-import Sound.OpenAL
-import Sound.OpenAL.AL.BasicTypes (ALsizei)
-import Sound.OpenAL.ALC.Capture
-import System.Exit (exitFailure)
-import System.IO (hPutStrLn, stderr)
-import qualified Data.Vector.Storable as V
-import qualified Data.Vector.Storable.Mutable as VM
+import Control.Concurrent (threadDelay)      --
+import Control.Exception  (bracket, finally) --
+import Control.Monad      (unless, when)     --
 
-import GHC.Float (float2Double)
+import Data.Int                --
+import Data.List (intersperse) --
+import Data.Word               --
 
--- import Sound.OpenAL.Proto.Conversion
--- import Sound.OpenAL.Proto.Types (sampleRate)
--- import Sound.OpenAL.Proto.UnitGen (sine)
+import Foreign                   --
+import Foreign.C.Types           --
+import Foreign.ForeignPtr        --
+import Foreign.ForeignPtr.Unsafe --
+import Foreign.Ptr               --
+
+import Sound.ALUT                --
+
+import Sound.OpenAL                         --
+import Sound.OpenAL.AL.BasicTypes (ALsizei) --
+import Sound.OpenAL.ALC.Capture             --
+
+import System.Exit (exitFailure)                    --
+import System.IO (hPutStrLn, stderr)                --
+import qualified Data.Vector.Storable as V          --
+import qualified Data.Vector.Storable.Mutable as VM --
+
+import GHC.Float (float2Double) --
 
 
 
@@ -64,7 +65,7 @@ type Sample = Double
 ---------------------------------------------------------------------------------------------------
 -- Data
 ---------------------------------------------------------------------------------------------------
-sampleRate = 44100 :: Int -- TODO: Don't hard-code sample rate
+samplerate = 44100 :: Int -- TODO: Don't hard-code sample rate
 
 mono8BufferSize    = bufferSize 1 (undefined :: Word8)
 mono16BufferSize   = bufferSize 1 (undefined :: Int16)
@@ -83,7 +84,7 @@ bufferSize nchannels sampleType secs = fromIntegral (numSamples secs) * sizeOf s
 
 -- |
 numSamples :: Double -> NumSamples
-numSamples secs = round (fromIntegral(sampleRate) * secs) :: NumSamples
+numSamples secs = round (fromIntegral(samplerate) * secs) :: NumSamples
 
 --------------------------------------------------------------------------------
 
@@ -92,7 +93,7 @@ sine :: Double -> [Sample]
 sine freq = cycle $ take n $ map sin [0, d..]
 	 where d = 2 * pi * freq / sr
 	       n = truncate (sr / freq)
-	       sr = fromIntegral sampleRate
+	       sr = fromIntegral samplerate
 
 --------------------------------------------------------------------------------
 
@@ -109,43 +110,35 @@ vecPtr = fst . VM.unsafeToForeignPtr0
 
 --------------------------------------------------------------------------------
 
-
 -- |
 -- captureSamples :: Device -> Ptr a -> NumSamples -> IO ()
 -- allocaBytes :: Int -> (Ptr a -> IO b) -> IO b
 -- allocaBytesAligned :: Int -> Int -> (Ptr a -> IO b) -> IO b
-withCaptureDevice :: (Maybe String) -> Double -> (Device -> IO c) -> IO c
-withCaptureDevice specifier secs action = bracket acquire finally action
+-- TODO: Find out why captureOpenDevice returns Nothing when secs is 0
+-- TODO: Pass in failure action (?)
+withCaptureDevice :: (Maybe String) -> Double -> (Device -> IO c) -> IO c -> IO c
+withCaptureDevice specifier secs onsuccess onfailure = acquire >>= \ mdevice -> case mdevice of
+		Just device -> bracket acquire finally onsuccess --
+		Nothing     -> onfailure                          --
 	where format      = Mono16
-	      acquire     = do (Just mic) <- captureOpenDevice specifier (fromIntegral sampleRate) format (numSamples secs)
-	                       captureStart mic
-	                       return mic
-	      finally mic = do captureStop mic
-	                       captureCloseDevice mic
+	      finally mic = captureStop mic >> captureCloseDevice mic
+	      record mic  = captureStart mic >> return mic
+	      acquire     = captureOpenDevice specifier (fromIntegral samplerate) format (numSamples secs)
 
 
 -- |
+-- TODO: Refactor
 -- capture :: V.Storable a => (Maybe String) -> Double -> IO (MemoryRegion a)
 -- capture :: Storable a => (Maybe String) -> Double -> (MemoryRegion a -> IO c) -> IO c
 -- capture :: (Maybe String) -> Double -> IO (V.Vector Int16)
 capture :: Maybe String -> Double -> (MemoryRegion CInt -> IO c) -> IO c -- According to GHCi
 capture specifier duration action = do
 	withCaptureDevice specifier duration $ \mic -> do
-		-- TODO: Why do we sleep for three seconds before recording?
-		-- Probably because we need to wait before invoking 'captureStop'
-		-- TODO: Handle delay more flexibly (move audio to a different thread?)
-		sleep $ realToFrac duration
-
-		-- mutableV <- V.thaw $ V.replicate (fromIntegral num) (fromIntegral 0)
-		mutableV <- V.thaw $ V.fromList $ map (pcm 16) $ take (fromIntegral num) $ sine 220
-		withForeignPtr (vecPtr mutableV) $ \ptr -> do
-			captureSamples mic ptr (fromIntegral num)
-		rec <- V.freeze mutableV
-
-		-- recordedBytes <- get $ captureNumSamples mic
-		-- putStrLn $ "Got samples: " ++ (show recordedBytes)
-
+		sleep $ realToFrac duration                                                          -- Sleep until we should stop recording
+		mutableV <- V.thaw . V.fromList . map (pcm 16) . take (fromIntegral num) $ sine 220  --
+		withForeignPtr (vecPtr mutableV) $ \ptr -> captureSamples mic ptr (fromIntegral num) --
+		rec <- V.freeze mutableV                                                             --
 		let (mem, size) = V.unsafeToForeignPtr0 rec
 		withForeignPtr mem $ \ptr -> action $ MemoryRegion ptr (fromIntegral size)
-	where num = (numSamples duration)
+	where num   = (numSamples duration)
 	      bytes = (mono16BufferSize duration)
